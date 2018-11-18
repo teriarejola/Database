21fe5547efd55859418552e9d3686821c1ae5ee5
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-18 16:19:22 -0500

    Update trigger2_add.sql

--- trigger2_add.sql
+++ trigger2_add.sql
@@ -1,4 +1,17 @@
 // A user may not bid on an item he or she is also selling
 
 PRAGMA foreign_keys= ON;
+
 drop trigger if exists trigger2;
+
+before insert on bids;
+
+when exists (
+  SELECT SellerID
+  FROM Items
+  WHERE new.UserID = SellerID)
+
+BEGIN 
+SELECT raise (rollback, 'You may not bid on an item you are selling.')
+END;
+
