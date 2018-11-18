0643b417415d47d22bb6d7f46cce1009029a488f
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-18 16:02:04 -0500

    Update trigger1_add.sql

--- trigger1_add.sql
+++ trigger1_add.sql
@@ -1,1 +1,14 @@
+PRAGMA foreign_keys= ON;
+
+drop trigger if exists trigger1;
+
+create trigger triger1
+after insert ON bid 
+for each row 
+
+BEGIN 
+    UPDATE Item 
+    SET Currently = new.Amount
+    WHERE Item_ID = new.Item_ID
+END;
 
