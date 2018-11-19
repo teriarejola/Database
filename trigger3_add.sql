d129299e2747e33e5340bd899b2ef39c30330088
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-19 18:15:21 -0500

    Update trigger3_add.sql

--- trigger3_add.sql
+++ trigger3_add.sql
@@ -1,18 +1,22 @@
+// No auction may have a bid before its start time or after its end time
+
 PRAGMA foreign_keys = ON;
 
 drop trigger if exists trigger3;
 
 create trigger trigger3
-
-after insert ON item 
+after insert ON Bids
 for each row
 when exists (
     SELECT *
     FROM Item 
     WHERE ItemID = new.ItemID AND 
-    new.Started >= new.Ends
-) 
+    new.Time < Started OR
+    new.Time > Ends
+)
 
 BEGIN 
-SELECT raise (rollback, 'An auction can't ends before it starts.')
+    SELECT raise (rollback, 'The bid should be made within the auction's start and end time.')
 END; 
+              
+              
