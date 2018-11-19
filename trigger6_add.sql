d4144229d49d4ddd7eff587f92c6a928dd442bc2
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-19 18:50:11 -0500

    Update trigger6_add.sql

--- trigger6_add.sql
+++ trigger6_add.sql
@@ -1,1 +1,21 @@
+// All new bids must be placed at the time which matches the current time of your AuctionBase system.
+
+PRAGMA foreign_keys = ON;
+
+drop trigger if exists trigger6;
+
+create trigger trigger6
+before insert ON Bids 
+for each row 
+
+when exists (
+  SELECT *
+	FROM Time
+	WHERE new.Time <> Time 
+)
+
+BEGIN 
+  SELECT raise (rollback, "The bid time should match the current time of system.");
+END;
+
 
