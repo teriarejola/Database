9792ef2662051e1ee51c395b8970c012f03c0bed
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-19 17:35:43 -0500

    Update trigger1_add.sql

--- trigger1_add.sql
+++ trigger1_add.sql
@@ -1,14 +1,16 @@
+// The Current Price of an item must always match the Amount of the most recent bid for that item
+
 PRAGMA foreign_keys= ON;
 
 drop trigger if exists trigger1;
 
 create trigger triger1
-after insert ON bid 
+after insert ON bids 
 for each row 
 
 BEGIN 
     UPDATE Item 
     SET Currently = new.Amount
-    WHERE ItemID = new.ItemID
+    WHERE ItemID = new.ItemID;
 END;
 
