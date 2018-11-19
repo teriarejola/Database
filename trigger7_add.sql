c72a98bdc7cd8fafd85a8fbac25d48e8ad9c00e9
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-19 18:56:42 -0500

    Update trigger7_add.sql

--- trigger7_add.sql
+++ trigger7_add.sql
@@ -1,1 +1,19 @@
+// The current time of your AuctionBase system can only advance forward in time, not backward in time
+
+PRAGMA foreign_keys = ON;
+
+drop trigger if exists trigger7;
+
+create trigger trigger7
+before update ON Time
+for each row 
+when exists ( 
+  SELECT * 
+  FROM Time 
+  WHERE new.Time <= Time
+) 
+
+BEGIN 
+  SELECT raise (rollback, "The current time of the system can only go forward. Update should be greater than the current time.");
+END;
 
