b7eef0d10cead3681d8f58de949b088200be5e34
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-19 18:38:40 -0500

    Update trigger5_add.sql

--- trigger5_add.sql
+++ trigger5_add.sql
@@ -1,2 +1,21 @@
+// Any new bid for a particular item must have a higher amount than any of the previous bids for that particular item
+
 PRAGMA foreign_keys = ON;
-drop trigger trigger5;
+
+drop trigger if exists trigger5;
+
+create trigger trigger5
+before insert ON Bids 
+for each row 
+
+when exists(
+	SELECT *
+	FROM Item
+	WHERE new.Amount <- Currently AND 
+  ItemID = new.ItemID
+)
+  
+BEGIN
+	SELECT raise (rollback, 'The bid amount must be greater than the highest bid for the item.');
+END;
+
