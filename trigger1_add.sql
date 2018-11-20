29742adb22d2ce8a43e07182fbeff5a211d35d99
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-19 19:28:38 -0500

    Update trigger1_add.sql

--- trigger1_add.sql
+++ trigger1_add.sql
@@ -1,4 +1,4 @@
-// The Current Price of an item must always match the Amount of the most recent bid for that item
+-- The Current Price of an item must always match the Amount of the most recent bid for that item
 
 PRAGMA foreign_keys= ON;
 
