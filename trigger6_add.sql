0050f0cbb1d7cd7a3cdef4e3690abac8caf45320
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-19 19:29:18 -0500

    Update trigger6_add.sql

--- trigger6_add.sql
+++ trigger6_add.sql
@@ -1,4 +1,4 @@
-// All new bids must be placed at the time which matches the current time of your AuctionBase system.
+-- All new bids must be placed at the time which matches the current time of your AuctionBase system.
 
 PRAGMA foreign_keys = ON;
 
