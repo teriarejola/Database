6c322e529ce0a139e4e0a6a96cf545707d5e9921
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-19 19:29:01 -0500

    Update trigger4_add.sql

--- trigger4_add.sql
+++ trigger4_add.sql
@@ -1,4 +1,4 @@
-// In every auction, the Number of Bids attribute corresponds to the actual number of bids for that particular item
+-- In every auction, the Number of Bids attribute corresponds to the actual number of bids for that particular item
 
 PRAGMA foreign_keys = ON;
 
