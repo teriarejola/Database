74da87d652e33fcb6e62e1dda4aa2ac7b6e859da
17genzo <45151669+17genzo@users.noreply.github.com>
2018-11-25 15:33:27 -0500

    Update verify-constraints.sql

--- verify-constraints.sql
+++ verify-constraints.sql
@@ -1,1 +1,16 @@
+SELECT *
+FROM Users
+WHERE NOT IN (SELECT sellerID
+              FROM Item) AND
+              NOT IN (SELECT bidderID
+                      FROM Bids);
 
+SELECT *
+FROM Item
+WHERE NOT IN (SELECT ItemID
+              FROM Bids);
+              
+SELECT *
+FROM Item
+WHERE NOT IN (SELECT ItemID
+              FROM Category);
