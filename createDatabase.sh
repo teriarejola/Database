8d5cc6a0300b3f1094b3fa28171b4029d3dae1f7
17genzo <45151669+17genzo@users.noreply.github.com>
2018-11-25 15:41:44 -0500

    Update createDatabase.sh

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,6 +1,6 @@
 sqlite3 AuctionBase.db < create.sql
 sqlite3 AuctionBase.db < load.txt
-sqlite3 AuctionBase.db < constraints_verify.sql
+sqlite3 AuctionBase.db < verify-constraints.sql
 sqlite3 AuctionBase.db < trigger1_add.sql
 sqlite3 AuctionBase.db < trigger1_drop.sql
 sqlite3 AuctionBase.db < trigger2_add.sql
