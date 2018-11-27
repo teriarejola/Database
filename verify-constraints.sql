3ecb4872a12834c6908716fcae5ea8ec95904aeb
teriarejola <31699263+teriarejola@users.noreply.github.com>
2018-11-26 23:38:33 -0500

    Update verify-constraints.sql

--- verify-constraints.sql
+++ verify-constraints.sql
@@ -1,3 +1,5 @@
+/* For #2 (All sellers and bidders must already exist as users) */
+
 SELECT sellerID
 FROM Item
 WHERE NOT IN (SELECT userID
@@ -7,12 +9,16 @@ SELECT bidderID
 FROM Bids
 WHERE NOT IN (SELECT userID
               FROM Users);
+              
+/* For #4 (Every bid must correspond to an actual item) */
 
 SELECT ItemID
 FROM Bids
 WHERE NOT IN (SELECT ItemID
               FROM Items);
               
+/* For #5 (The items for a given category must all exist) */
+
 SELECT ItemID
 FROM Category
 WHERE NOT IN (SELECT ItemID
